812ccc3dd8d8fa8adbd37f95c7c3fa0ab88af8ec
Daniel Brantner <daniel.brantner@protonmail.com>
2024-02-25 11:23:11 +0100

    feat: Add character base class

--- Source/BudgetVikings/Characters/BVCharacter.h
+++ Source/BudgetVikings/Characters/BVCharacter.h
@@ -1,11 +1,10 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
+#include "CharacterBase.h"
 #include "Logging/LogMacros.h"
 #include "BVCharacter.generated.h"
+
 
 class USpringArmComponent;
 class UCameraComponent;
@@ -13,34 +12,30 @@ class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
 
+
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
 
+
 UCLASS(config=Game)
-class ABVCharacter : public ACharacter
+class ABVCharacter : public ACharacterBase
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 	
-	/** MappingContext */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputMappingContext* DefaultMappingContext;
 
-	/** Jump Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* JumpAction;
 
-	/** Move Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* MoveAction;
 
-	/** Look Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
 	
@@ -51,15 +46,16 @@ class ABVCharacter : public ACharacter
 	UInputAction* SprintAction;
 
 public:
+	
 	ABVCharacter();
 	
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 protected:
 
-	/** Called for movement input */
 	void Move(const FInputActionValue& Value);
 
-	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
 
 	UFUNCTION(BlueprintCallable)
@@ -67,18 +63,9 @@ protected:
 
 	UFUNCTION(BlueprintCallable)
 	void ToggleSprinting(const FInputActionValue& Value);			
-
-protected:
-	// APawn interface
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
-	// To add mapping context
-	virtual void BeginPlay();
-
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	virtual void BeginPlay() override;
 };
 
--- Source/BudgetVikings/Characters/CharacterBase.h
+++ Source/BudgetVikings/Characters/CharacterBase.h
@@ -1,23 +1,23 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "CharacterBase.h"
-#include "MainMenuCharacter.generated.h"
+#include "GameFramework/Character.h"
+#include "CharacterBase.generated.h"
 
 
 UCLASS()
-class BUDGETVIKINGS_API AMainMenuCharacter : public ACharacterBase
+class BUDGETVIKINGS_API ACharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
 	
-	AMainMenuCharacter();
+	ACharacterBase();
 	
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
 protected:
 	
 	virtual void BeginPlay() override;
